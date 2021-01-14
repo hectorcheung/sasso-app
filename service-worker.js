790c9bdb21c1cfea08ffc12c94b177087dda81ae
hectorcheung <69161902+hectorcheung@users.noreply.github.com>
2021-01-13 23:22:35 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sasso-app/precache-manifest.b2c249a7a9a9025cc7a189071fe45724.js"
+  "/sasso-app/precache-manifest.a3e5a2fb68db5da5c9e48443dae964f3.js"
 );
 
 self.addEventListener('message', (event) => {
